6d5fc7c088e0ba08e16066fecfbcf16fdfbf9e48
Mihajlo Micic <m2micic@uwaterloo.ca>
2026-02-25 23:43:57 -0500

    chore: close control HTTP server on shutdown

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,8 @@ const run = async () => {
   }
 
   if (config.CONTROL_HTTP_PORT > 0) {
-    await createHttpServer(control, config, config.CONTROL_HTTP_PORT, createLogger('runtime.http', config.LOG_LEVEL as any));
-    runtimeHandles.push({ close: async () => {} });
+    const httpServer = await createHttpServer(control, config, config.CONTROL_HTTP_PORT, createLogger('runtime.http', config.LOG_LEVEL as any));
+    runtimeHandles.push({ close: async () => {\n      await new Promise<void>((resolve, reject) => {\n        httpServer.close((error) => {\n          if (error) {\n            reject(error);\n            return;\n          }\n          resolve();\n        });\n      });\n    }});
   }
 
   const socketServer = createSocketServer(control, config, createLogger('runtime.socket', config.LOG_LEVEL as any));
